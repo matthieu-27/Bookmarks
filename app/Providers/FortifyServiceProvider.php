a66fe731eda6912c81da8da662fae42a16e31f48
matthieu-27 <matthieu27@pm.me>
2022-09-13 15:03:43 +0200

    Add views
    Proper project start commit.
    Fortify/Sanctum installed and configured.
    Basic views for login/register

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -14,36 +14,42 @@ use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		Fortify::createUsersUsing(CreateNewUser::class);
+		Fortify::updateUserProfileInformationUsing(
+			UpdateUserProfileInformation::class
+		);
+		Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+		Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+		Fortify::registerView("auth.register");
+		Fortify::loginView(function () {
+			return view("auth.login");
+		});
 
-        RateLimiter::for('login', function (Request $request) {
-            $email = (string) $request->email;
+		RateLimiter::for("login", function (Request $request) {
+			$email = (string) $request->email;
 
-            return Limit::perMinute(5)->by($email.$request->ip());
-        });
+			return Limit::perMinute(5)->by($email . $request->ip());
+		});
 
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        });
-    }
+		RateLimiter::for("two-factor", function (Request $request) {
+			return Limit::perMinute(5)->by($request->session()->get("login.id"));
+		});
+	}
 }
